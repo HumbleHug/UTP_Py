90b9066d1a2aef4e93b92ae6ed934b68a5c0aecb
HumbleHug <sirax.gameplays@gmail.com>
2025-09-09 10:54:43 -0500

    updates

--- S2_TryExcept.py
+++ S2_TryExcept.py
@@ -1,4 +1,5 @@
 valor_txt = input("ingrese los valores de Temperatura en C: ")
+
 try: #para posibles errores en datos de ingreso
     t=float(valor_txt)
     if t >= 10: #condicion if "condicion 1": para el ambito matematico
